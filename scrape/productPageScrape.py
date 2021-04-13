302491a411104d60deb4253c3759952c271f47f8
horix7 <horix77777@gmail.com>
2021-04-13 23:49:41 +0200

    adding product name price and description

--- scrape/productPageScrape.py
+++ scrape/productPageScrape.py
@@ -12,11 +12,14 @@ print(driver.title)
 
 productImages = driver.find_elements_by_class_name("product-single__thumb")
 
-productTittle = driver.find_element_by_class_name("product-single__title product-title-big ")
+productTittle = driver.find_element_by_class_name("product-single__title product-title-big").get_attribute("innerText")
+productDescription = driver.find_element_by_class_name("product-single__description")
+productPrice = driver.find_element_by_class_name("money").get_attribute("innerText")
+
 
 images = [image.get_attribute("src") for image in productImages]
 
-print(images)
+print(productTittle)
 time.sleep(2)
 
 driver.quit()
