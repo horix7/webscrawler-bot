a47f3921628e02486654d0422285c9823b8fded4
horix7 <horix77777@gmail.com>
2021-04-13 23:46:38 +0200

    adding product tittle

--- scrape/productPageScrape.py
+++ scrape/productPageScrape.py
@@ -12,6 +12,8 @@ print(driver.title)
 
 productImages = driver.find_elements_by_class_name("product-single__thumb")
 
+productTittle = driver.find_element_by_class_name("product-single__title product-title-big ")
+
 images = [image.get_attribute("src") for image in productImages]
 
 print(images)
