b57d4c1f825c8868c406f8d110d57d715d4e543f
horix7 <horix77777@gmail.com>
2021-04-15 13:29:37 +0200

    scraping all products

--- scrape/productPageScrape.py
+++ scrape/productPageScrape.py
@@ -3,36 +3,41 @@ from selenium.webdriver.common.keys import Keys
 import time
 import json 
 
+allProductsLinks = json.load(open("data.json"))
+
 PATH = "/home/paul/Documents/chromedriver"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://internationaldeliver.shop/products/wmns-air-jordan-1-mid-se-dutch-green")
-print(driver.title)
+productArray = list()
 
-productImages = driver.find_elements_by_class_name("product-single__photo")
+for everyLink in allProductsLinks:
+    driver.get(everyLink["link"])
 
-productTittle = driver.find_element_by_class_name("product-single__title").get_attribute("innerText")
-productDescription = driver.find_element_by_class_name("product-single__description").get_attribute("innerText")
-productPrice = driver.find_element_by_class_name("money").get_attribute("innerText")
+    productImages = driver.find_elements_by_class_name("product-single__photo")
+
+    productTittle = driver.find_element_by_class_name("product-single__title").get_attribute("innerText")
+    productDescription = driver.find_element_by_class_name("product-single__description").get_attribute("innerText")
+    productPrice = driver.find_element_by_class_name("money").get_attribute("innerText")
 
 
-images = [image.get_attribute("src").split("?v=")[0] for image in productImages]
+    images = [image.get_attribute("src").split("?v=")[0] for image in productImages]
 
 
-productInfo = dict()
-productInfo["title"] = "".join(productTittle.split("\n"))
-productInfo["price"] = productPrice.split("RF")[0]
-productInfo["description"] = productDescription
-setimages =  [i for j, i in enumerate(images) if i not in images[:j]]
-setimages[0] = setimages[1]
-setimages[1] = setimages[0]
-productInfo["imageUrl"] = setimages
+    productInfo = dict()
+    productInfo["title"] = "".join(productTittle.split("\n"))
+    productInfo["price"] = productPrice.split("RF")[0]
+    productInfo["description"] = productDescription
+    setimages =  [i for j, i in enumerate(images) if i not in images[:j]]
+    setimages[0] = setimages[1]
+    setimages[1] = setimages[0]
+    productInfo["imageUrl"] = "".join(setimages)
+    productInfo["collection"] = everyLink["collection"]
 
-print(len(setimages))
-with open('product2.json', 'w') as f:
-    json.dump(productInfo, f)
+    productArray.append(productInfo)
 
+with open('allProducts.json', 'w') as f:
+    json.dump(productArray, f)
 
 
 time.sleep(2)
