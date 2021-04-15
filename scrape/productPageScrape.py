e6530bc232d946c1b492dfc47cdce2f08a851efb
horix7 <horix77777@gmail.com>
2021-04-15 11:57:45 +0200

    adding set of images and right address

--- scrape/productPageScrape.py
+++ scrape/productPageScrape.py
@@ -7,28 +7,30 @@ PATH = "/home/paul/Documents/chromedriver"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://internationaldeliver.shop/collections/nike-dunk/products/dunk-low-sb-am90-infrared")
+driver.get("https://internationaldeliver.shop/products/wmns-air-jordan-1-mid-se-dutch-green")
 print(driver.title)
 
-productImages = driver.find_elements_by_class_name("product-single__thumb")
+productImages = driver.find_elements_by_class_name("product-single__photo")
 
 productTittle = driver.find_element_by_class_name("product-single__title").get_attribute("innerText")
 productDescription = driver.find_element_by_class_name("product-single__description").get_attribute("innerText")
 productPrice = driver.find_element_by_class_name("money").get_attribute("innerText")
 
 
-images = [image.get_attribute("src") for image in productImages]
+images = [image.get_attribute("src").split("?v=")[0] for image in productImages]
 
-print(productDescription)
 
 productInfo = dict()
 productInfo["title"] = "".join(productTittle.split("\n"))
-productInfo["price"] = int(productPrice.split("RF")[0])
+productInfo["price"] = productPrice.split("RF")[0]
 productInfo["description"] = productDescription
-productInfo["imageUrl"] = images
+setimages =  [i for j, i in enumerate(images) if i not in images[:j]]
+setimages[0] = setimages[1]
+setimages[1] = setimages[0]
+productInfo["imageUrl"] = setimages
 
-
-with open('product.json', 'w') as f:
+print(len(setimages))
+with open('product2.json', 'w') as f:
     json.dump(productInfo, f)
 
 
